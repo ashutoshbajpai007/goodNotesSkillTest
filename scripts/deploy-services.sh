8a01828b36ee2f4507155cbc8220aecbbcf065de
ashutoshbajpai007 <ashutosh.bajpai00007@gmail.com>
2026-01-12 22:54:05 +0530

    Initial commit: CI load test setup with Helm, KinD, and reusable scripts

--- scripts/deploy-services.sh
+++ scripts/deploy-services.sh
@@ -13,10 +13,6 @@ helm repo update
 helm install ingress-nginx ingress-nginx/ingress-nginx \
   --namespace ingress-nginx \
   --create-namespace \
-  --set-string controller.nodeSelector.ingress-ready=true \
-  --set controller.tolerations[0].key=node-role.kubernetes.io/master \
-  --set controller.tolerations[0].effect=NoSchedule \
-  --set controller.tolerations[0].operator=Exists \
   --set controller.service.type=LoadBalancer
 
 # Wait for ingress-nginx controller to be ready before proceeding
