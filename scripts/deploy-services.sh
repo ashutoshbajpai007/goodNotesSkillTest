750335485a7f29f82547b1f7e9c7c5ec5dcd94eb
ashutoshbajpai007 <ashutosh.bajpai00007@gmail.com>
2026-01-12 22:23:31 +0530

    Initial commit: CI load test setup with Helm, KinD, and reusable scripts

--- scripts/deploy-services.sh
+++ scripts/deploy-services.sh
@@ -11,13 +11,11 @@ helm repo update
 
 # Install ingress-nginx via Helm
 helm install ingress-nginx ingress-nginx/ingress-nginx \
-  --set controller.nodeSelector."ingress-ready"=true \
+  --set controller.nodeSelector.ingress-ready="true" \
   --set controller.tolerations[0].key=node-role.kubernetes.io/master \
   --set controller.tolerations[0].effect=NoSchedule \
   --set controller.tolerations[0].operator=Exists \
-  --set controller.service.type=NodePort \
-  --set controller.service.nodePorts.http=80 \
-  --set controller.service.nodePorts.https=443
+  --set controller.service.type=LoadBalancer
 
 # Install echo-app via Helm
 helm install echo-app $HELM_CHART_PATH
