5768ee4fdb2bcbe266271c5eb6ce4e2e9cc272a9
ashutoshbajpai007 <ashutosh.bajpai00007@gmail.com>
2026-01-12 23:39:26 +0530

    Initial commit: CI load test setup with Helm, KinD, and reusable scripts

--- scripts/deploy-services.sh
+++ scripts/deploy-services.sh
@@ -13,7 +13,8 @@ helm repo update
 helm install ingress-nginx ingress-nginx/ingress-nginx \
   --namespace ingress-nginx \
   --create-namespace \
-  --set controller.service.type=ClusterIP
+  --set controller.service.type=NodePort \
+  --set controller.service.nodePorts.http=80
 
 # Wait for ingress-nginx controller to be ready before proceeding
 kubectl wait --namespace ingress-nginx --for=condition=ready pod --selector=app.kubernetes.io/component=controller --timeout=300s
